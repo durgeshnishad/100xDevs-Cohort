92f15b1983f966cbff36e0f93eae436711168c8f
Durgesh Nishad <dnishad042@gmail.com>
2024-06-07 17:23:23 +0530

    body-parser

--- http-server/basic.js
+++ http-server/basic.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
+const bodyParser= require('body-parser')
 const port = 3000
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/postreq', (req, res) => {
+  console.log(req.body)
+  console.log(req.header)
+  res.send('Hello from Post method!')
 })
 
 app.listen(port, () => {
