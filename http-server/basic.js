5dc42b9d1c62b4f0d6e5194b44d6241724f8da21
Durgesh Nishad <dnishad042@gmail.com>
2024-06-07 17:43:06 +0530

    query-params

--- http-server/basic.js
+++ http-server/basic.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser= require('body-parser')
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 
@@ -9,10 +9,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/postreq', (req, res) => {
+app.post('/postreq1', (req, res) => {
   console.log(req.body)
   console.log(req.header)
-  res.send('Hello from Post method!')
+  res.send('Hello from Post method/body-parser!')
+})
+
+app.post('/postreq2', (req, res) => {
+  console.log(req.query.message)
+  res.send('Hello from Post method/query params!')
 })
 
 app.listen(port, () => {
